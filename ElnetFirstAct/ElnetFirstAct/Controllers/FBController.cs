db882b4fe95dc3b87e52969949574f14097ff364
juba0713 <basasrepo@gmail.com>
2024-02-17 15:24:44 +0800

    Push Again

---
+++ ElnetFirstAct/ElnetFirstAct/Controllers/FBController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ElnetFirstAct.Controllers
+{
+    public class FBController : Controller
+    {
+        public IActionResult Login()
+        {
+            return PartialView("Login");
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
--- ElnetFirstAct/ElnetFirstAct/Program.cs
+++ ElnetFirstAct/ElnetFirstAct/Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -18,6 +19,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=FB}/{action=Login}/{id?}");
 
 app.Run();
